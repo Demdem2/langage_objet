bc7983a2eccceebd793dacbf89ad49fcfe2c4fb6
Demdem2 <c.demange@ludus-academie.com>
2022-05-05 10:05:42 +0200

    core programme cpp v1

---
+++ langage_objet/main.cpp
@@ -0,0 +1,10 @@
+/*limite du C: lorsqu on manipule type, on ne peut pas dépendre des types structurer.les vecteurs: on peut faire des opération avec.*/
+
+//Fichier souce: main.cpp
+
+#include <stdio.h>
+
+int main() {
+	printf_s("version 1.0 du programme.\ncpp ");
+	return 0;
+}
