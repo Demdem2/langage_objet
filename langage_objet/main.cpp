2961cba430b2b3b62df6a86ad3108c685880448d
Demdem2 <c.demange@ludus-academie.com>
2022-05-05 10:05:42 +0200

    Ajoutez des fichiers projet.

---
+++ langage_objet/main.cpp
@@ -0,0 +1,8 @@
+/*limite du C: lorsqu on manipule type, on ne peut pas dépendre des types structurer.les vecteurs: on peut faire des opération avec.*/
+
+//Fichier souce: main.cpp
+
+
+int main() {
+
+}
